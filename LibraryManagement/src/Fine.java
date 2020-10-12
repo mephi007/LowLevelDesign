bfabfb9b49855f39a2d78bb726b96bca6bbfef3c
mephi007 <roysumit949@gmail.com>
2020-10-12 15:31:49 +0530

    Added Fine Class

---
+++ LibraryManagement/src/Fine.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class Fine {
+	private Date creationDate;
+	private double bookItemBarCode;
+	private String memberId;
+	
+	public static void collectFine(String memberId, long days) {
+		
+	}
+}
